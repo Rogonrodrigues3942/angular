b9af32df630e860e7cf08b4ec16e2a8ef40b40f2
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-09-23 20:22:18 -0300

    Updating file: "main.ts" and "PLANO-ESTUDOS_ANGULAR.TXT".

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,5 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule) //^Atenção ao parâmetro "AppModule".
   .catch(err => console.error(err));
